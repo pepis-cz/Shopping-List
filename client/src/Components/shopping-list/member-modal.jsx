c3b9ffa53495fb047f2a66f360596d183e7b276b
pepis-cz <petr.duc@seznam.cz>
2025-11-02 03:19:33 +0100

    zlepšování frontendu
    implementace validace a přidání id do array

--- client/src/Components/shopping-list/member-modal.jsx
+++ client/src/Components/shopping-list/member-modal.jsx
@@ -1,45 +1,83 @@
+import ListGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
-import ListGroup from 'react-bootstrap/ListGroup'
+import Form from 'react-bootstrap/Form'
 
-function MemberModal({show = false, onHide = () => {}, list}) {
+function Modal({ members }) {
+    const [show, setShow] = useState(false);
+    const [array, setArray] = useState(members);
+
+    const onHide = () => setShow(false);
+
+    //kontrola zda existuje id
+    const saveName = () => setEditingId(null);
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            saveName();
+        }
+    }
+
+    const handleRemove = (id) => {
+        setArray((prev) => prev.filter((item) => item !== id));
+    }
+
     return (
         <>
-            <Modal show = {show} onHide = {onHide}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Members</Modal.Title>
-                </Modal.Header>
 
-                <Modal.Body>
-                    <ListGroup>
-                        <ListGroup.Item>{list.owner} Owner</ListGroup.Item>
+            <Button onClick = {() => setShow(true)}>
+                <i className = 'bi bi-person'/>
+            </Button>
 
-                        {list?.members?.map((member) => {
-                            <ListGroup.Item key = {member.id}>
-                                <>
-                                    {member.email}
-                                </>
+            {show &&
+                <Modal onHide = {onHide}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Spolupracovníci</Modal.Title>
+                    </Modal.Header>
 
-                                <>
-                                    <Button>
-                                        <i className = 'bi bi-x-lg'/>
-                                    </Button>
-                                </>
+                    <Modal.Body>
+                        <ListGroup>
+                            {array.map((member) => (
+                                <ListGroup.Item key = {member.id}>
+                                    <>
+                                        <i className = 'bi bi-person'/>
+                                        {member.email}
+                                        {owner && <span>(Vlastník)</span>}
+                                    </>
+
+                                    {userId === owner && member.id !== owner &&(
+                                        <Button onClick = {() => handleRemove(member.id)}>
+                                            <i className = 'bi bi-x-lg'/>
+                                        </Button>
+                                    )}
+                                </ListGroup.Item>
+                            ))}
+
+                            <ListGroup.Item>
+                                {owner &&
+                                    <Form>
+                                        <Form.Control
+                                            type = 'email'
+                                            placeholder = 'E-mailová adresa pro sdílení'
+
+                                            onBlur = {saveName}
+                                            onKeyDown = {handleKeyDown}
+                                        />
+                                    </Form>
+                                }
                             </ListGroup.Item>
-                        })}
-
-                    </ListGroup>
+                        </ListGroup>
 
 
-                </Modal.Body>
+                    </Modal.Body>
 
-                <Modal.Footer>
-                    <Button variant = 'primary' onClick={onHide}>Save</Button>
-                    <Button variant = 'secondary' onClick={onHide}>Close</Button>
-                </Modal.Footer>
-            </Modal>
+                    <Modal.Footer>
+                        <Button variant = 'secondary' onClick={onHide}>Zavřít</Button>
+                    </Modal.Footer>
+                </Modal>
+            }
         </>
     )
 }
 
-export default MemberModal
+export default Modal
